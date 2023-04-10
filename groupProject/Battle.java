e6ef1d33e7f5343b58d86610fbf93a9a2e3f3b26
Jayden Remillard <127774024+bppwd2024-remillard@users.noreply.github.com>
2023-04-10 13:56:56 -0400

    Add files via upload

--- groupProject/Battle.java
+++ groupProject/Battle.java
@@ -7,9 +7,10 @@ import java.util.ArrayList;
 import java.awt.image.BufferedImage; 
 import java.io.IOException; 
 import javax.imageio.ImageIO;
- //indiviual encounters are loaded on the screen they appear on.
+ 
 public class Battle extends Screen{ 
 	private BattlePlayer bp1; 
+	private BasicEnemy e1;
 	private BasicEnemy activeEnemy;
 	private ArrayList<BasicEnemy> enemies;
 	private BufferedImage image; 
@@ -25,15 +26,15 @@ public class Battle extends Screen{
 	int select;
 	int enemySelect;
 	
-	public Battle(BattlePlayer bp1, ArrayList<BasicEnemy> enemies, Screen returnScreen){  
+	public Battle(BattlePlayer bp1, BasicEnemy e1, Screen returnScreen){  
 		myColour = new Color(0, 0,0,0);	
-		this.enemies=enemies;
 		this.bp1=bp1;
-		activeEnemy = enemies.get(0);
-		for( BasicEnemy c: enemies) {
-			objects.add(c);
-		}
+		this.e1=e1;
+		activeEnemy = e1;
+		enemies = new ArrayList<>();
+		enemies.add(e1);
 		objects.add(bp1); 
+		objects.add(e1);
 		select=1;
 		enemySelect=0;
 		this.returnScreen=returnScreen;		
@@ -47,52 +48,7 @@ public class Battle extends Screen{
 		}catch (IOException e){ 
 			e.printStackTrace();
 		}
-	}  
-	public Battle(BattlePlayer bp1, BasicEnemy activeEnemy, Screen returnScreen){ 
-
-		    myColour = new Color(0, 0,0,0);
-		
-		    this.bp1=bp1;
-		
-		    this.activeEnemy=activeEnemy;
-		
-		    activeEnemy = this.activeEnemy;
-		
-		    enemies = new ArrayList<>();
-		
-		    enemies.add(activeEnemy);
-		
-		    objects.add(bp1);
-		
-		    objects.add(activeEnemy);
-		
-		    select=1;
-		
-		    enemySelect=0;
-		
-		    this.returnScreen=returnScreen;  
-		
-		    try{
-		
-		      image=ImageIO.read(getClass().getResourceAsStream("/sprites/battleBackground1.png"));
-		
-		      select1=ImageIO.read(getClass().getResourceAsStream("/sprites/actions-Fight.png"));
-		
-		      select2=ImageIO.read(getClass().getResourceAsStream("/sprites/actions-Run.png"));
-		
-		      select3=ImageIO.read(getClass().getResourceAsStream("/sprites/actions-Item.png"));
-		
-		      select4=ImageIO.read(getClass().getResourceAsStream("/sprites/actions-Skill.png"));
-		
-		      arrow=ImageIO.read(getClass().getResourceAsStream("/sprites/arrow.png"));
-		
-		    }catch (IOException e){
-		
-		     e.printStackTrace();
-		
-		    }
-		
-		  } 
+	}   
 	public void update(){ 
 		super.update();
 		if(select>=5){ 
@@ -113,7 +69,7 @@ public class Battle extends Screen{
 		else if(select==4){ 
 			activeSelect=select4;
 		} 
-		if(activeEnemy.getHealth()<=0){ 
+		if(e1.getHealth()<=0){ 
 			//Win winScreen=new Win(returnScreen);
 			bp1.maxHeal();
 			MyGame.activeScreen=returnScreen;
@@ -127,10 +83,10 @@ public class Battle extends Screen{
 		Sign name = new Sign(((activeEnemy.x+(activeEnemy.x+activeEnemy.width))/2)-33, activeEnemy.y-25, 50, 20, myColour, "", Color.BLACK, 12);
 		objects.add(name);
 		pen.drawImage(bp1.returnImage(),bp1.getX(),bp1.getY(), 100,100,null); 
-		pen.drawImage(activeEnemy.returnImage(),activeEnemy.getX(),activeEnemy.getY(), 100,100,null);
+		pen.drawImage(e1.returnImage(),e1.getX(),e1.getY(), 100,100,null);
 		pen.drawImage(activeSelect,0,500,200,200,null);
-		//pen.drawString("HP: " + activeEnemy.health, ((activeEnemy.x + (activeEnemy.x+activeEnemy.width))/2) - 20, activeEnemy.y - 3);
-		pen.drawString("HP: " + activeEnemy.health, ((activeEnemy.x + (activeEnemy.x+activeEnemy.width))/2) - 20, activeEnemy.y - 3); 
+		pen.drawString("HP: " + bp1.health, ((bp1.x + (bp1.x+bp1.width))/2) - 20, bp1.y);
+		pen.drawString("HP: " + e1.health, ((e1.x + (e1.x+e1.width))/2) - 20, e1.y - 3); 
 		if(select == 1 && control == KeyEvent.VK_ENTER || select == 1 && control == KeyEvent.VK_SPACE) {
 			pen.setFont(new Font("Comic Sans MS", Font.BOLD, 30));
 			pen.drawImage(arrow, ((activeEnemy.x+(activeEnemy.x+activeEnemy.width))/2)-25, activeEnemy.y+activeEnemy.height+10, 50, 100, null);
@@ -139,8 +95,8 @@ public class Battle extends Screen{
 	} 
 	
 	public void keyTyped(KeyEvent ke) {
-		//bp1.enemyAttack(activeEnemy.attack);
-		//activeEnemy.playerAttack(2,5);
+		//bp1.enemyAttack(e1.attack);
+		//e1.playerAttack(2,5);
 		control=ke.getKeyChar();  
 		if(control=='s'){
 			select+=1;
