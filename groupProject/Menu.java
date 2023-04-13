2198afd4b7aaf8436d3805cff0ea8283f9d630a5
porpoiseQueen <114865745+porpoiseQueen@users.noreply.github.com>
2023-04-13 14:21:17 -0400

    Add files via upload

---
+++ groupProject/Menu.java
@@ -0,0 +1,14 @@
+import java.awt.Color;
+import java.awt.Graphics;
+public class Menu<Item> extends GameObject{ 
+    Item option;
+    public Menu(int x, int y, int width, int height, Color c) {
+        super(x,y,width,height,c);
+    }
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
