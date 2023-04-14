f23559b7d8997d40d4f868c032010b3869195cb8
porpoiseQueen <114865745+porpoiseQueen@users.noreply.github.com>
2023-04-14 08:26:32 -0400

    menuy

--- groupProject/Menu.java
+++ groupProject/Menu.java
@@ -1,14 +1,27 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
 public class Menu<Item> extends GameObject{ 
-    Item option;
-    public Menu(int x, int y, int width, int height, Color c) {
+    private Item option;
+    private Item[] options;
+    private ArrayList<Item> optionsArrayList;
+    public Menu(int x, int y, int width, int height, Color c, Item[] stuff) {
         super(x,y,width,height,c);
+        options=stuff;
+        option=options[0];
     }
+    public Menu(int x, int y, int width, int height, Color c, ArrayList<Item> stuff) {
+        super(x,y,width,height,c);
+        optionsArrayList=stuff;
+        option=optionsArrayList.get(0);
+    }
+
     @Override
     public void update() {
         // TODO Auto-generated method stub
         
     }
-    
+    public void draw(Graphics pen) {
+        
+    }
 }
