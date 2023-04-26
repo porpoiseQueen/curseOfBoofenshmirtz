d4e13ddd3864b7f746aa7f94c43b2cf1a6b96000
Lillie Hampton <114865745+porpoiseQueen@users.noreply.github.com>
2023-04-26 09:13:56 -0400

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,34 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+public class Menu<I> extends GameObject{ 
+    private I option;
+    private I[] options;
+    private ArrayList<I> optionsArrayList;
+    public Menu(int x, int y, int width, int height, Color c, I[] stuff) {
+        super(x,y,width,height,c);
+        options=stuff;
+        option=options[0];
+    }
+    public Menu(int x, int y, int width, int height, Color c, ArrayList<I> stuff) {
+        super(x,y,width,height,c);
+        optionsArrayList=stuff;
+        option=optionsArrayList.get(0);
+
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        
+    }
+    public void draw(Graphics pen) {
+        
+    }
+}
+class Item<I> {
+      protected Item<I> left;
+      protected Item<I> right;
+      protected Item<I> up;
+      protected Item<I> down;
+}
