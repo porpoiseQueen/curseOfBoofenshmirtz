591d29f9ed3fe173f42fc93ba433cb63c8c9c869
Jayden Remillard <ja.d.remillard@baypath.net>
2023-05-02 08:55:52 -0400

    1

---
+++ groupProject/Background.java
@@ -0,0 +1,9 @@
+import java.awt.Color;
+public class Background extends GameObject{  
+	public Background(int x, int y, int width, int height, Color c){ 
+		super(x,y,width,height,c);
+	} 
+	public void update(){ 
+		
+	}
+}
