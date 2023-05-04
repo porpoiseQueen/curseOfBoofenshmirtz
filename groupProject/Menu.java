d85b20789a4f50cccbfa27c68542567deb37f68f
Jayden Remillard <ja.d.remillard@baypath.net>
2023-05-04 08:51:49 -0400

    a

--- groupProject/Menu.java
+++ groupProject/Menu.java
@@ -2,15 +2,15 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
 public class Menu<I> extends GameObject{ 
-    private I option;
-    private I[] options;
-    private ArrayList<I> optionsArrayList;
-    public Menu(int x, int y, int width, int height, Color c, I[] stuff) {
+    private Item<I> option;
+    private Item<I>[] options;    //String[]
+    private ArrayList<Item<I>> optionsArrayList;    //ArrayList<String>
+    public Menu(int x, int y, int width, int height, Color c, Item<I>[] stuff) {
         super(x,y,width,height,c);
         options=stuff;
         option=options[0];
     }
-    public Menu(int x, int y, int width, int height, Color c, ArrayList<I> stuff) {
+    public Menu(int x, int y, int width, int height, Color c, ArrayList<Item<I>> stuff) {
         super(x,y,width,height,c);
         optionsArrayList=stuff;
         option=optionsArrayList.get(0);
@@ -23,28 +23,30 @@ public class Menu<I> extends GameObject{
         
     }
     public void draw(Graphics pen) {
-        /* if(optionsArrayList.isEmpty()) {
+        if(optionsArrayList.isEmpty()) {
             for(int i = 0; i < options.length; i++) {
                 pen.setColor(options[i].getBgColor());
-                pen.fillRect(options[i].getPosX(), options[i].getY(), width, height);
+                pen.fillRect(options[i].getX(), options[i].getY(), width, height);
                 pen.setColor(options[i].getTextColor());
                 pen.drawString(options[i].getName(), options[i].getX(), options[i].getY());
             }
         } else {
             for(int i = 0; i < optionsArrayList.size(); i++) {
                 pen.setColor(optionsArrayList.get(i).getBgColor());
-                pen.fillRect(optionsArrayList.get(i).getPosX(), optionsArrayList.get(i).getY(), width, height);
+                pen.fillRect(optionsArrayList.get(i).getX(), optionsArrayList.get(i).getY(), width, height);
                 pen.setColor(optionsArrayList.get(i).getTextColor());
                 pen.drawString(optionsArrayList.get(i).getName(), optionsArrayList.get(i).getX(), optionsArrayList.get(i).getY());
             }
-        } */
+        }
     }
 }
-class Item<I> {
+class Item<I> {   //Item<String>
     private int positionX, positionY;
     private Item<I> up, down, left, right;
     private Color bgColor, textColor;
-    public Item(int posX, int posY, Item<I> up, Item<I> down, Item<I> left, Item<I> right, Color bg, Color txt) {
+    private I value;
+    private String name;
+    public Item(I value, String name, int posX, int posY, Item<I> up, Item<I> down, Item<I> left, Item<I> right, Color bg, Color txt) {
         positionX = posX;
         positionY = posY;
         this.up = up;
@@ -53,7 +55,10 @@ class Item<I> {
         this.right = right;
         bgColor = bg;
         textColor = txt;
+        this.value = value;
+        this.name = name;
     }
+    public String getName() { return name; }
     public int getX() { return positionX; }
     public int getY() { return positionY; }
     public Item<I> getUp() { return up; }
@@ -62,4 +67,5 @@ class Item<I> {
     public Item<I> getRight() { return right; }
     public Color getBgColor() { return bgColor; }
     public Color getTextColor() { return textColor; }
+    public I getValue() { return value; }
 }
